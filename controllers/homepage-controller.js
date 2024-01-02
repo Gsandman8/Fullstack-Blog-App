389f9106a79d50f4c1fb0e34635e1d72d3c0394f
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-02 14:34:51 -0800

    Created homepage route and controller

---
+++ controllers/homepage-controller.js
@@ -0,0 +1,37 @@
+const {Post, User, Comment} = require('../models');
+
+function homepageController(req, res){
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(postData => {
+        const posts = postData.map(post => post.get({plain: true}));
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+}
+
+module.exports = homepageController;
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,9 +4,14 @@ const signupRoutes = require('./signup-routes');
 const dashboardRoutes = require('./dashboard-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const homepageController = require('../../controllers/homepage-controller.js');
 
 router.use('/login', loginRoutes);
 router.use('/signup', signupRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/post', postRoutes);
 router.use('/comment', commentRoutes);
+
+router.get('/', homepageController);
+
+module.exports = router;
